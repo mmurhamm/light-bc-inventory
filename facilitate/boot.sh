d108ee6ff5177ed2c9585efc83b2e0bda22444f0
kitty-catt <kitty-catt@example.com>
2020-09-10 12:41:52 +0200

    make exec

--- facilitate/boot.sh
+++ facilitate/boot.sh
@@ -4,22 +4,22 @@
 #export DOCKER_EMAIL=''
 
 # Git Section
-export GIT_USERNAME='<user>'
+export GIT_USERNAME='kitty-catt'
 
 # SonarQube Server Section
 # Login to SonarQube Server, make a project and generate a token for it.
 export SONARQUBE_URL='http://sonarqube-sonarqube.tools.svc.cluster.local:9000'
-export SONARQUBE_PROJECT='<project>'
-export SONARQUBE_LOGIN='<token>'
+export SONARQUBE_PROJECT='bc-light-inventory'
+export SONARQUBE_LOGIN='7904fb70bc5d1fd4c01fcac93351d9b8577dc0d0'
 
 # The target namespace or project in OpenShift
 export BC_PROJECT="bc-light"
 
 # ICR with VA Scan
-export IBM_ID_APIKEY='<apikey>'
-export IBM_ID_EMAIL='<email>'
+export IBM_ID_APIKEY=IO0y6D49IUwXVDfMmx8KWN5pw1WDUUVe9qPKfDu1bGsv
+export IBM_ID_EMAIL=ronald.van.de.kuil@nl.ibm.com
 export IBM_REGISTRY_URL='de.icr.io'
-export IBM_REGISTRY_NS='<namespace>'
+export IBM_REGISTRY_NS=kitty-catt
 
 # Quay registry with vulnerability scan
 # export QUAY_USERNAME=''
